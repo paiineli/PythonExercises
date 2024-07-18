4ac5ffdeac6ac7620b581155620e0883670a6bf6
Lucas Paineli <painelilucas@gmail.com>
2024-07-17 22:43:14 -0300

    create repository

---
+++ Repository/ChangeVariables.py
@@ -0,0 +1,13 @@
+# variable swap exercise
+
+x = input("Enter the value of x: ")
+y = input("Enter the value of y: ")
+
+# create a temporary variable
+
+temp = x
+x = y
+y = temp
+
+print(f"The value of x after the swap is: {x}")
+print(f"The value of y after the swap is: {y}")
---
+++ Repository/VerifyingNumberSigns.py
@@ -0,0 +1,10 @@
+# verifying number signs exercise
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print(f"The number {number} is positive")
+elif number == 0:
+    print(f"The number {number} is zero")
+else:
+    print(f"The number {number} is negative")
